68e949da1c96d91dbe937f94570d2d834d9ab544
Aman Kumar <amanalphakumar@gmail.com>
2021-11-08 23:48:26 +0530

    Create Binary_Tree_Level_Order_Traversal.cpp

---
+++ B/Binary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,44 @@
+// Return level order traversal of BT as a 2D matrix
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+// Aman Kumar
+
+vector<vector<int>> levelOrder(TreeNode* root) 
+{
+    vector<vector<int>> ret ;
+
+    queue<TreeNode*> q ;
+
+    if( root == NULL )
+        return ret ;
+    q.push(root) ;
+
+    while( !q.empty() )
+    {
+        vector<int> row ;
+        queue<TreeNode*> q2 ;
+
+        while( !q.empty() )
+        {
+            TreeNode *qfront = q.front() ;
+
+            q2.push(qfront);
+            row.push_back( qfront -> val );
+            q.pop() ;
+        }
+
+        while( !q2.empty() )
+        {
+            TreeNode *curr = q2.front() ;
+            q2.pop() ;
+
+            if( curr -> left != NULL )
+                q.push( curr -> left ) ;
+            if( curr -> right != NULL )
+                q.push( curr -> right ) ;
+        }
+
+        ret.push_back(row) ;
+    }
+
+    return ret ;
+}
