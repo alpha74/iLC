bc7ba2fc9e70ef84d14c0fcd8cce73a8bdfb0668
Aman Kumar <amanalphakumar@gmail.com>
2022-01-19 21:49:14 +0530

    Update Two_Sum.cpp

--- T/Two_Sum.cpp
+++ T/Two_Sum.cpp
@@ -2,66 +2,21 @@
 // https://leetcode.com/problems/two-sum/
 // Aman Kumar
 
-vector<int> twoSum(vector<int>& nums, int target) 
+vvector<int> twoSum(vector<int>& nums, int target) 
 {
-    vector<int> ret ;
-    int size = nums.size() ;
+    vector<int> ret = {-1, -1} ;
 
     map<int,int> hash ;
 
-    // Set hash map
-    for( int i = 0 ; i < size ; i++ )
+    for( int i = 0 ; i < nums.size() ; i++ )
     {
-        hash[ nums[i] ]++ ;
-    }
-
-    bool found = false ;
-    for( int i = 0 ; i < size && !found ; i++ )
-    {
-        int diff = target - nums[i] ;
-
-        // If diff is same as nums[i], then there should be two diffs
-        if( diff == nums[i] )
+        if( hash.find( target - nums[i] ) != hash.end() )
         {
-            if( hash[diff] > 1 )
-            {
-                found = true ;
-                ret.push_back( diff ) ;
-                ret.push_back( diff ) ;
-            }
+            ret[0] = hash[ target - nums[i] ] ;
+            ret[1] = i ;
+            return ret ;
         }
-        else if( hash[diff] >= 1 )
-        {
-            found = true ;
-            ret.push_back( diff ) ;
-            ret.push_back( nums[i] ) ;
-        }
-    }
-
-    // Find actual indices
-    if( ret.size() > 0 )
-    {
-        found = false ;
-
-        for( int i = 0 ; i < size && !found ; i++ )
-        {
-            if( nums[i] == ret[0] )
-            {
-                ret[0] = i ;
-                found = true ;
-            }
-        }
-
-        found = false ;
-
-        for( int i = 0 ; i < size && !found ; i++ )
-        {
-            if( nums[i] == ret[1] && i != ret[0] )
-            {
-                ret[1] = i ;
-                found = true ;
-            }
-        }
+        hash[ nums[i] ] = i ;
     }
 
     return ret ;
