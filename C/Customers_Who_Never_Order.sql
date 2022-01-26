83ff8fdcb99167664d805f7c2552511084aa3633
Aman Kumar <amanalphakumar@gmail.com>
2022-01-26 20:17:29 +0530

    Create Customers_Who_Never_Order.sql

---
+++ C/Customers_Who_Never_Order.sql
@@ -0,0 +1,14 @@
+// SQL : Get names of customers who have not ordered anytime
+// https://leetcode.com/problems/customers-who-never-order/
+// Aman Kumar
+
+# Using subquery to get cutomer who have orders and then removing them from all customers list
+SELECT Customers.name as Customers
+FROM Customers
+WHERE Customers.id NOT IN (
+ SELECT Customers.id
+ FROM Customers FULL JOIN Orders
+ ON Customers.id = Orders.customerId
+ GROUP by Customers.name
+ HAVING COUNT(*) > 0
+)
