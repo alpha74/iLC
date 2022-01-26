7c8b8706797a263b22858bb3c30e2414507132fd
Aman Kumar <amanalphakumar@gmail.com>
2022-01-26 17:16:28 +0530

    Create Merge_Two_Sorted_Lists.cpp

---
+++ M/Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,56 @@
+// Merge two sorted Linked lists
+// https://leetcode.com/problems/merge-two-sorted-lists/
+// Aman Kumar
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+{
+    ListNode *newHead = new ListNode(-1) ;
+    ListNode *temp = newHead ;
+
+    while( list1 != NULL && list2 != NULL )
+    {
+        if( list1 -> val < list2 -> val )
+        {
+            temp -> next = list1 ;
+            list1 = list1 -> next ;
+            temp = temp -> next ;
+        }
+        else if( list2 -> val < list1 -> val )
+        {
+            temp -> next = list2 ;
+            temp = temp -> next ;
+            list2 = list2 -> next ;
+        }
+        else
+        {
+            temp -> next = list1 ;
+            temp = temp -> next ;
+            list1 = list1 -> next ;
+
+            temp -> next = list2 ;
+            temp = temp -> next ;
+            list2 = list2 -> next ;
+        }
+    }
+
+    while( list1 != NULL )
+    {
+        temp -> next = list1 ;
+        temp = temp -> next ;
+        list1 = list1 -> next ;
+    }
+
+    while( list2 != NULL )
+    {
+        temp -> next = list2 ;
+        temp = temp -> next ;
+        list2 = list2 -> next ;
+    }
+
+    temp -> next = NULL ;
+    temp = newHead ;
+    newHead = newHead -> next ;
+    delete temp ;
+
+    return newHead ;
+}
