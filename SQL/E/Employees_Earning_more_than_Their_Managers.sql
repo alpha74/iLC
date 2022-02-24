78c27fda823757414c1f75547e00cae7516c2389
Aman Kumar <amanalphakumar@gmail.com>
2022-02-24 23:24:48 +0530

    Update Employees_Earning_more_than_Their_Managers.sql

--- SQL/E/Employees_Earning_more_than_Their_Managers.sql
+++ SQL/E/Employees_Earning_more_than_Their_Managers.sql
@@ -1,3 +1,7 @@
+# Employees earning more salary than their managers
+# https://leetcode.com/problems/employees-earning-more-than-their-managers/
+# Aman Kumar
+
 SELECT E1.name AS Employee
 FROM Employee AS E1
 
