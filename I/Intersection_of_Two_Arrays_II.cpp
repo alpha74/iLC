b36e70949e4c034356b9d4ce0191ad891672b3a7
Aman Kumar <amanalphakumar@gmail.com>
2022-03-10 23:18:42 +0530

    Create Intersection_of_Two_Arrays_II.cpp

---
+++ I/Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,32 @@
+// Intersection of two arrays: return same occurence of numbers
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+// Aman Kumar
+
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
+{
+    unordered_map<int,int> hash1 ;
+    unordered_map<int,int> hash2 ;
+
+    for( int i = 0 ; i < nums1.size() ; i++ )
+    {
+        hash1[ nums1[i] ]++ ;
+    }
+    for( int i = 0 ; i < nums2.size() ; i++ )
+    {
+        hash2[ nums2[i] ]++ ;
+    }
+
+    vector<int> ret ;
+
+    for( int i = 0 ; i < nums1.size() ; i++ )
+    {
+        if( hash1[ nums1[i] ] > 0 && hash2[ nums1[i] ] > 0 )
+        {
+            ret.push_back( nums1[i] ) ;
+            hash1[ nums1[i] ]-- ;
+            hash2[ nums1[i] ]-- ;
+        }
+    }
+
+    return ret ;
+}
