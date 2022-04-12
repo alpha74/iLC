a38c0add8333ced3811b8edd865ba11e645ac897
Aman Kumar <amanalphakumar@gmail.com>
2022-04-12 23:55:10 +0530

    Create Sum_Root_to_Leaf_Numbers.cpp

---
+++ S/Sum_Root_to_Leaf_Numbers.cpp
@@ -0,0 +1,36 @@
+// Return sum of all numbers represented as digits from root to leaf
+// 
+// Aman Kumar
+
+void checkPathSum( TreeNode *root, int currSum, int &ret, int row )
+{
+    if( root == NULL )
+        return ;
+
+    currSum = (currSum * 10 ) + root -> val ;
+
+    if( root -> left == NULL && root -> right == NULL )
+    {
+        ret += currSum ;
+
+        return ;
+    }
+
+    if( root -> left != NULL )
+        checkPathSum(root->left, currSum, ret, row ) ;
+
+    if( root -> right != NULL )
+        checkPathSum( root->right, currSum , ret, row ) ;
+}
+
+int sumNumbers(TreeNode* root) 
+{
+    int ret = 0 ;
+    int curr = 0 ;
+
+    if( root == NULL )
+        return ret ;
+
+    checkPathSum( root, 0, ret, curr ) ;    
+    return ret ;    
+}   
