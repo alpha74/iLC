fc52aa393c38642cc3a71bcaa6d03a5d95e2a0fc
Aman Kumar <amanalphakumar@gmail.com>
2022-04-14 23:38:25 +0530

    Create Valid_Anagram.cpp

---
+++ V/Valid_Anagram.cpp
@@ -0,0 +1,38 @@
+// Check if given strings are anagrams of each other
+// https://leetcode.com/problems/valid-anagram/
+// Aman Kumar
+
+bool isAnagram(string s, string t) 
+{
+    if( s.length() != t.length() )
+        return false ;
+
+    vector<int> smap( 27, 0 ) ;
+    vector<int> tmap( 27, 0 ) ;
+
+    for( int i = 0 ; i < s.length() ; i++ )
+    {
+        smap[ s[i] -'a']++ ;
+    }
+
+    for( int i = 0 ; i < t.length() ; i++ )
+    {
+        tmap[ t[i] -'a']++ ;
+    }
+
+    bool ret = true ;
+
+    for( int i = 0 ; i < s.length() && ret ; i++ )
+    {
+        if( smap[ s[i] -'a'] != tmap[ s[i] -'a'] )
+            ret = false ;
+    }
+
+    for( int i = 0 ; i < t.length() && ret ; i++ )
+    {
+        if( smap[ s[i] -'a'] != tmap[ s[i] -'a'] )
+            ret = false ;
+    }
+
+    return ret ;
+}
