7e400dac123da0aa940a2bbe268ef05f6dc45740
Aman Kumar <amanalphakumar@gmail.com>
2022-05-26 22:52:00 +0530

    Create Number_Complement.cpp

---
+++ N/Number_Complement.cpp
@@ -0,0 +1,19 @@
+// Return 1s complement of a number
+// https://leetcode.com/problems/number-complement/submissions/
+// Aman Kumar
+
+int findComplement(int num) 
+{
+    int ret = 0 ;
+
+    for( int i = 0 ; num != 0 ; i++)
+    {
+        int b = ( num & 1 ) ^ 1 ;
+
+        ret = ret + b * pow( 2, i ) ;
+
+        num = num >> 1 ;
+    }
+
+    return ret ;
+}
