39a66a9b682810efe7a7118eab6a038deb0c714d
Aman Kumar <amanalphakumar@gmail.com>
2022-06-19 20:11:07 +0530

    Create Nim_Game.cpp

---
+++ N/Nim_Game.cpp
@@ -0,0 +1,19 @@
+// Return true if you can win Nim Game
+// https://leetcode.com/problems/nim-game/
+// Aman Kumar
+
+bool canWinNim(int n) 
+{
+    return n % 4 ;
+
+/*
+3
+5
+6
+7
+8
+9
+10
+11
+*/
+}
