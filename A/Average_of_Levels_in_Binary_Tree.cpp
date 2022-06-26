f6f5262d1eda1c5454d7419b5f59a516943d0744
Aman Kumar <amanalphakumar@gmail.com>
2022-06-26 21:23:52 +0530

    Create Average_of_Levels_in_Binary_Tree.cpp

---
+++ A/Average_of_Levels_in_Binary_Tree.cpp
@@ -0,0 +1,60 @@
+// Return list of average of levels of binary tree
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/
+// Aman Kumar
+
+vector<double> averageOfLevels(TreeNode* root) 
+{
+    vector<double> ret ;
+
+    queue<TreeNode*> q1, q2 ;
+
+    q1.push( root ) ;
+
+    while( !q1.empty() || !q2.empty() )
+    {
+        if( !q1.empty() )
+        {
+            double sum = 0 ;
+            int count = 0 ;
+
+            while( !q1.empty() )
+            {
+                TreeNode *curr = q1.front() ;
+                q1.pop() ;
+
+                sum += curr -> val ;
+                count++ ;
+
+                if( curr -> left != NULL )
+                    q2.push( curr -> left ) ;
+                if( curr -> right != NULL )
+                    q2.push( curr -> right ) ;
+            }
+
+            ret.push_back( double( (1.0 * sum) / count )) ;
+        }
+        else if( !q2.empty() )
+        {
+            double sum = 0 ;
+            int count = 0 ;
+
+            while( !q2.empty() )
+            {
+                TreeNode *curr = q2.front() ;
+                q2.pop() ;
+
+                sum += curr -> val ;
+                count++ ;
+
+                if( curr -> left != NULL )
+                    q1.push( curr -> left ) ;
+                if( curr -> right != NULL )
+                    q1.push( curr -> right ) ;
+            }
+
+            ret.push_back( double( (1.0 * sum) / count )) ;
+        }
+    }
+
+    return ret ;
+}
