2713b3016988b4c49d43aba2d4cb0a39eb75976b
Aman Kumar <amanalphakumar@gmail.com>
2022-07-25 23:36:36 +0530

    Create Maximum_Product_Difference_between_Two_Pairs.cpp

---
+++ M/Maximum_Product_Difference_between_Two_Pairs.cpp
@@ -0,0 +1,40 @@
+// Find max product difference between 2 pairs
+// https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+// Aman Kumar
+
+int maxProductDifference(vector<int>& nums) 
+{
+    int index1 = 0 ;
+    int min1 = 10001, min2 = 10001 ;
+
+    // Find largest
+    int index2 = 0 ;
+    int max1 = -1, max2 = -1 ;
+
+    // Find min1
+    for( int i = 0 ; i < nums.size() ; i++ )
+    {
+        if( nums[i] < min1 )
+        {
+            min1 = nums[i] ;
+            index1 = i ;
+        }
+        if( nums[i] > max1 )
+        {
+            index2 = i ;
+            max1 = nums[i] ;
+        }
+    }
+
+    // Find min2
+    for( int i = 0 ; i < nums.size() ; i++ )
+    {
+        if( i != index1 && nums[i] < min2 )
+            min2 = nums[i] ;
+
+        if( i != index2 && nums[i] > max2 ) 
+            max2 = nums[i] ;
+    }
+
+    return (max1 * max2) - (min1 * min2) ;
+}
