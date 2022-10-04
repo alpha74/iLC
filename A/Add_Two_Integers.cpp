f13454bc8f06be02df80687d92729125bbd2f593
Aman Kumar <amanalphakumar@gmail.com>
2022-10-04 23:12:31 +0530

    Create Add_Two_Integers.cpp

---
+++ A/Add_Two_Integers.cpp
@@ -0,0 +1,10 @@
+// Add two integers
+// https://leetcode.com/problems/add-two-integers/
+// Aman Kumar
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Add Two Integers.
+// Memory Usage: 5.8 MB, less than 73.05% of C++ online submissions for Add Two Integers.
+int sum(int num1, int num2) 
+{
+    return num1 + num2  ;
+}
