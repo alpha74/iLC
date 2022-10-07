15a619c184e6edd46c903d3b2a6640d9a83c2fea
Aman Kumar <amanalphakumar@gmail.com>
2022-10-07 20:34:41 +0530

    Update Sales_Analysis_III.sql

--- SQL/S/Sales_Analysis_III.sql
+++ SQL/S/Sales_Analysis_III.sql
@@ -2,18 +2,11 @@
 # https://leetcode.com/problems/sales-analysis-iii/
 # Aman Kumar
 
-# Runtime: 1825 ms, faster than 27.64% of MySQL online submissions for Sales Analysis III.
+# Runtime: 1497 ms, faster than 62.83% of MySQL online submissions for Sales Analysis III.
 # Memory Usage: 0B, less than 100.00% of MySQL online submissions for Sales Analysis III.
-SELECT DISTINCT(S1.product_id) AS product_id, 
-( 
-    SELECT P1.product_name FROM Product P1
-    WHERE P1.product_id = S1.product_id
-) AS product_name
 
-FROM Sales S1
-WHERE S1.sale_date >= "2019-01-01" AND S1.sale_date <= "2019-03-31"
-AND S1.product_id NOT IN ( 
-    SELECT S2.product_id
-    FROM Sales S2
-    WHERE S2.sale_date NOT BETWEEN "2019-01-01" AND "2019-03-31"
-) ;
+SELECT Product.product_id, Product.product_name 
+FROM Product JOIN Sales 
+ON Product.product_id = Sales.product_id 
+GROUP BY Sales.product_id 
+HAVING MIN(Sales.sale_date) >= "2019-01-01" AND MAX(Sales.sale_date) <= "2019-03-31";
