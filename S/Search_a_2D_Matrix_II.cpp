65ac7ea0c9cfa180e37fc54f1726aa7a0a5e6860
Aman Kumar <amanalphakumar@gmail.com>
2022-11-20 22:55:06 +0530

    Create Search_a_2D_Matrix_II.cpp

---
+++ S/Search_a_2D_Matrix_II.cpp
@@ -0,0 +1,34 @@
+// Search a 2D matrix for a target number, which is sorted row-wise and col-wise in ascending order
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+// Aman Kumar
+
+// Runtime: 87 ms, faster than 96.47% of C++ online submissions for Search a 2D Matrix II.
+// Memory Usage: 14.7 MB, less than 98.72% of C++ online submissions for Search a 2D Matrix II.
+bool searchMatrix(vector<vector<int>>& matrix, int target) 
+{
+    int n = matrix.size() ;
+    int m = matrix[0].size() ;
+
+    int i = 0, j = m-1 ;
+
+    bool ret = false ;
+
+    while( i < n && j >= 0 && !ret )
+    {
+        int curr = matrix[i][j] ;
+
+        if( target == curr )
+            ret = true ;
+        else if( curr > target )
+            j-- ;
+        else
+            i++ ;
+    }
+
+    return ret ;
+/*
+[[-1,3]]
+3
+
+*/
+}
