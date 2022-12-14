d95e132bb995fa82dc27ff7c867d5f73f61ab5c0
Aman Kumar <amanalphakumar@gmail.com>
2022-12-14 23:41:24 +0530

    Create Remove_Nodes_From_Linked_List.cpp

---
+++ R/Remove_Nodes_From_Linked_List.cpp
@@ -0,0 +1,72 @@
+// Remove every node which has a node having value > current node value
+// https://leetcode.com/problems/remove-nodes-from-linked-list/description/
+// Aman Kumar
+
+// Runtime 508 ms Beats 85.56%
+// Memory 174 MB Beats 28.11%
+ListNode* removeNodes(ListNode* head) 
+{
+    vector<int> rightMax ;
+
+    ListNode *temp = head ;
+
+    while( temp != NULL )
+    {
+        rightMax.push_back( temp -> val ) ;
+        temp = temp -> next ;
+    }
+
+    int n = rightMax.size() ;
+    int currMax = rightMax[n-1] ;
+
+    // Create right max array
+    for( int i = n-2 ; i >= 0 ; i-- )
+    {
+        if( rightMax[i] < currMax )
+            rightMax[i] = currMax ;
+
+        else if( rightMax[i] > currMax )
+            currMax = rightMax[i] ;
+    }
+
+    ListNode *newhead = head ;
+    int index = 0 ;
+
+    temp = head ;
+
+    // For new head node
+    while( head != NULL && head -> val < rightMax[index])
+    {
+        temp = head ;
+        head = head -> next ;
+        delete temp ;
+        index++ ;
+    }
+
+    newhead = head ;
+    temp = newhead ;
+    head = head -> next ;
+    index++ ;
+
+    while( head != NULL )
+    {
+        if( head -> val < rightMax[index] )
+        {
+            // Store previous node
+            temp -> next = head -> next ;
+
+            ListNode *del = head ;
+            head = head -> next ;
+
+            delete del ;
+        }
+        else
+        {
+            temp = head ;
+            head = head -> next ;
+        }
+        index++ ;
+    }
+
+    return newhead ;
+}
