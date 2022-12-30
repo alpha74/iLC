99bb1eba67dc0fa4f2e1958030d5dfaa0814512b
Aman Kumar <amanalphakumar@gmail.com>
2022-12-30 23:23:33 +0530

    Update Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp

--- M/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
+++ M/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
@@ -2,32 +2,36 @@
 // https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
 // Aman Kumar
 
-// Runtime 122 ms Beats 54.35%
-// Memory 8.8 MB Beats 89.53%
+// Runtime 4 ms Beats 95.48%
+// Memory 9.8 MB Beats 7.20%
+
 vector<int> minOperations(string boxes) 
 {
-    int n = boxes.length() ;
-    vector<int> onePos ;
+    int n = boxes.size();
 
-    for( int i = 0 ; i < n ; i++ )
+    vector<int> leftOnes(n, 0);
+    vector<int> leftCost(n, 0);
+
+    vector<int> rightOnes(n, 0);
+    vector<int> rightCost(n, 0);
+
+    vector<int> ans(n, 0);
+
+    for( int i = 0 ; i < n; i++ ) 
     {
-        if( boxes[i] == '1')
-        onePos.push_back( i ) ;
-    }    
-
-    vector<int> ret(n, 0) ;
-
-    for( int i = 0 ; i < n ; i++ )
-    {
-        int count = 0 ;
-
-        for( int j = 0 ; j < onePos.size() ; j++ )
-        {
-            count += abs( i - onePos[j] ) ;
-        }
-
-        ret[i] = count ;
+        leftCost[i] = i ? (leftCost[i - 1] + leftOnes[i - 1]) : 0;
+        leftOnes[i] = (i ? leftOnes[i - 1] : 0) + (boxes[i] == '1' ? 1 : 0);
     }
 
-    return ret ;
+    for( int i = n - 1 ; i >= 0 ; i-- ) 
+    {
+        rightCost[i] = i < n - 1 ? (rightCost[i + 1] + rightOnes[i + 1]) : 0;
+        rightOnes[i] = (i < n - 1 ? rightOnes[i + 1] : 0) + (boxes[i] == '1' ? 1 : 0);
+    }
+
+    for (int i = 0; i < n; ++i) 
+    {
+        ans[i] = leftCost[i] + rightCost[i];
+    }
+    return ans;
 }
