5722a4f89cf7edef234141fbe2f5a87324d89835
Aman Kumar <amanalphakumar@gmail.com>
2023-02-26 20:58:12 +0530

    Update Max_Subarray.cpp

--- M/Max_Subarray.cpp
+++ M/Max_Subarray.cpp
@@ -2,6 +2,8 @@
 // https://leetcode.com/problems/maximum-subarray/
 // Aman Kumar
 
+// Runtime 124 ms Beats 50.64%
+// Memory 67.7 MB Beats 63.37%
 int maxSubArray(vector<int>& nums) 
 {
     int i = 0, j = 1 ;
