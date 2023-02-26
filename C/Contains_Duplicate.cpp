3c5b419ee02a0b6c938cb7ae96b1ec53182b1ddc
Aman Kumar <amanalphakumar@gmail.com>
2023-02-26 20:55:42 +0530

    Update Contains_Duplicate.cpp

--- C/Contains_Duplicate.cpp
+++ C/Contains_Duplicate.cpp
@@ -2,15 +2,18 @@
 // https://leetcode.com/problems/contains-duplicate/
 // Aman Kumar
 
+// Runtime 134 ms Beats 63.6%
+// Memory 69.5 MB Beats 34.66%
 bool containsDuplicate(vector<int>& nums) 
 {
-    unordered_map<int,bool> hash ;
+    unordered_set<int> hash ;
 
     for( int i = 0 ; i < nums.size() ; i++ )
     {
-        if( hash[ nums[i] ] == true )
+        if( hash.find( nums[i]) != hash.end() )
             return true ;
-        hash[ nums[i] ] = true ;
+
+        hash.insert( nums[i] ) ;
     }
     return false ;
 }
