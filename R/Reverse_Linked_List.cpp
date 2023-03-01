1608937a538d27a50a72b60cf3e0ef1441eab61d
Aman Kumar <amanalphakumar@gmail.com>
2023-03-01 13:06:23 +0530

    Update Reverse_Linked_List.cpp

--- R/Reverse_Linked_List.cpp
+++ R/Reverse_Linked_List.cpp
@@ -2,6 +2,8 @@
 // https://leetcode.com/problems/reverse-linked-list/
 // Aman Kumar
 
+// Runtime 4 ms Beats 77.22%
+// Memory 8.4 MB Beats 51.80%
 ListNode* reverseList(ListNode* head) 
 {
     if( head == NULL || head -> next == NULL )
