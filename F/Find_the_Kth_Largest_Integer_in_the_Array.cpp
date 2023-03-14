98b110687d522b65390f88956c14539057e876ae
Aman Kumar <amanalphakumar@gmail.com>
2023-03-14 14:17:44 +0530

    Update Find_the_Kth_Largest_Integer_in_the_Array.cpp

--- F/Find_the_Kth_Largest_Integer_in_the_Array.cpp
+++ F/Find_the_Kth_Largest_Integer_in_the_Array.cpp
@@ -2,26 +2,52 @@
 // https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/description/
 // Aman Kumar
 
-// Runtime 486 ms Beats 19.4%
-// Memory 197.8 MB Beats 16.76%
-static bool sortStr(string a, string b)
+// Runtime 192 ms Beats 95.50%
+// Memory 77.2 MB Beats 41.71%
+struct sortStr
 {
-    if (a.size() != b.size()) 
-        return a.size() < b.size();
-    return a < b ;
-}
+        bool operator()(string &a, string &b)
+        {
+            if (a.size() != b.size()) 
+                return a.size() > b.size();
+            return a > b ;
+        }
+    };
 
-string kthLargestNumber(vector<string>& nums, int k) 
-{
-    make_heap( nums.begin(), nums.end(), sortStr ) ;
 
-    while(k-- > 1)
+class Solution {
+public:
+    bool isSmaller(string a, string b)
     {
-        pop_heap( nums.begin(), nums.end(), sortStr) ;
-        nums.pop_back() ;
-    }  
+        if (a.size() != b.size()) 
+            return a.size() < b.size();
+        return a < b ;
+    }
 
-    return nums.front() ;
+    string kthLargestNumber(vector<string>& nums, int k) 
+    {
+        priority_queue<string, vector<string>, sortStr> minheap ;
+        int curr = 0 ;
+
+        for( int i = 0 ; i < nums.size() ; i++ )
+        {
+            if(curr < k)
+            {
+                minheap.push( nums[i] ) ;
+                curr++ ;
+            }
+            else if( isSmaller(minheap.top(), nums[i] ) == 1)
+            {
+                minheap.pop();
+                minheap.push( nums[i] ) ;
+            }
+
+            if(curr > k)
+                minheap.pop() ;
+        }
+
+        // Return min string
+        return minheap.top() ;
 
 /*
 ["21","2","12","982"]
@@ -30,4 +56,5 @@ string kthLargestNumber(vector<string>& nums, int k)
 ["423","521","2","42"]
 2
 */
-}
+    }
+};
