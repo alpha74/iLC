28a6f827d82dc53d50463e6eaddbe8f05a82a553
Aman Kumar <amanalphakumar@gmail.com>
2023-04-28 15:36:12 +0530

    Update Bulb_Switcher.cpp

--- B/Bulb_Switcher.cpp
+++ B/Bulb_Switcher.cpp
@@ -11,3 +11,15 @@ int bulbSwitch(int n)
 
     return sqrt(n) ;
 }
+/*
+The solution is based on the observation that a bulb will be toggled 
+an even number of times (including zero) if it has an even number of divisors, 
+and an odd number of times if it has an odd number of divisors. 
+
+A bulb will have an odd number of divisors if and only if it is a perfect square, 
+because each divisor less than the square root of the number corresponds to 
+a divisor greater than the square root of the number. 
+Therefore, the number of bulbs that are on after n rounds is equal to 
+the number of perfect squares less than or equal to n, 
+which is equal to the floor of the square root of n.
+*/
