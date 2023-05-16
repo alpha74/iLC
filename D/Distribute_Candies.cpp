b4546239ca86c78d62addcff4dc0b1899c0701d1
Aman Kumar <amanalphakumar@gmail.com>
2023-05-16 13:33:22 +0530

    Update Distribute_Candies.cpp

--- D/Distribute_Candies.cpp
+++ D/Distribute_Candies.cpp
@@ -2,6 +2,8 @@
 // https://leetcode.com/problems/distribute-candies/
 // Aman Kumar
 
+// Runtime 297 ms Beats 61.54%
+// Memory 116 MB Beats 66.12%
 int distributeCandies(vector<int>& candyType) 
 {
     unordered_map<int,bool> hash ;
