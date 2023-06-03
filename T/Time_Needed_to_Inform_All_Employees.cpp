a5a62d0184a4eeb7c4df89ab0c3a4986d3fa4954
Aman Kumar <amanalphakumar@gmail.com>
2023-06-04 00:04:56 +0530

    Update Time_Needed_to_Inform_All_Employees.cpp

--- T/Time_Needed_to_Inform_All_Employees.cpp
+++ T/Time_Needed_to_Inform_All_Employees.cpp
@@ -3,8 +3,95 @@
 // https://leetcode.com/problems/time-needed-to-inform-all-employees/
 // Aman Kumar
 
-// SOL-1 500ms 180MB
+// SOL-2 
+// Runtime 357 ms Beats 36.75%
+// Memory 135.5 MB Beats 26.42%
+class MultiNode {
+    public:
+        int id ;
+        vector<MultiNode*> child ;
+    
+        MultiNode(int i)
+        {
+            id = i ;
+        }
+} ;
 
+class Solution {
+public:
+    int getInformTime( MultiNode *root, vector<int> &iTime, int c)
+    {
+        if( (root -> child).size() == 0 )
+            return c ;
+        
+        int maxTime = c ;
+        int size = (root -> child).size() ;
+
+        for( int i = 0 ; i < size ; i++ )
+        {        
+            maxTime = max( maxTime, getInformTime( (root -> child)[i], iTime, c+iTime[ root -> id ] ) ) ;
+        }
+        return maxTime ;
+    }
+    
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime)
+    {
+        MultiNode *root = new MultiNode( headID ) ;
+        
+        vector<MultiNode*> hash(n,NULL) ;
+        
+        hash[ headID ] = root ;
+        
+        // Create new nodes
+        for( int i = 0 ; i < n ; i++ )
+        {
+            if( i != headID )
+            {
+                MultiNode *newnode = new MultiNode(i) ;
+                hash[i] = newnode ;
+            }
+        }
+        
+        // Traverse again and connect parent child nodes
+        for( int i = 0 ; i < n ; i++ )
+        {
+            if( i != headID )
+            {
+                MultiNode *currNode = hash[i] ;
+                
+                // Get manager of current node
+                MultiNode *rootNode = hash[ manager[i] ] ;
+                
+                (rootNode -> child).push_back( currNode ) ;
+            }
+        }
+        
+        int maxTime = 0 ;
+        int size = (root -> child).size() ;
+        
+        for( int i = 0 ; i < size ; i++ )
+        {
+            maxTime = max( maxTime, getInformTime( (root -> child)[i], informTime, informTime[headID]) ) ;
+        }
+        
+        return maxTime ;
+/*
+6
+2
+[3,2,-1,2,2,2]
+[0,0,1,2,0,0]
+
+15
+0
+[-1,0,0,1,1,2,2,3,3,4,4,5,5,6,6]
+[1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
+*/
+    }
+};
+
+
+
+// SOL-1 500ms 180MB -----------------
 class MultiNode {
     public:
         int id ;
