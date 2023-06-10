1e45b1f8143e5add0948e0da3eff1382c6b4876b
Aman Kumar <amanalphakumar@gmail.com>
2023-06-11 00:41:05 +0530

    Create Return_length_of_Arguments_Passed.js

---
+++ JS/R/Return_length_of_Arguments_Passed.js
@@ -0,0 +1,12 @@
+// Return length of arguents
+// https://leetcode.com/problems/return-length-of-arguments-passed/description/
+// Aman Kumar
+
+// Runtime 55 ms Beats 74.99%
+// Memory 41.6 MB Beats 89.47%
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
