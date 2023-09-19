d5071121c27bf43b4f511d09430a940726a43d47
Aman Kumar <amanalphakumar@gmail.com>
2023-09-19 23:55:37 +0530

    Update Find_the_Duplicate_Number.cpp

--- F/Find_the_Duplicate_Number.cpp
+++ F/Find_the_Duplicate_Number.cpp
@@ -2,6 +2,36 @@
 // https://leetcode.com/problems/find-the-duplicate-number/
 // Aman Kumar
 
+// Runtime 71 ms Beats 92.98%
+// Memory 61.5 MB Beats 30.42%
+int findDuplicate(vector<int>& nums) 
+{
+    int slow = 0;
+    int fast = 0;
+
+    do
+    {
+        slow = nums[slow] ;
+        fast = nums[fast] ;
+        fast = nums[fast] ;
+    } while(slow != fast);
+
+    // Find the meet point of slow and fast
+    slow = 0 ;
+
+    while(slow != fast)
+    {
+        // Move with same speed
+        slow = nums[slow] ;
+        fast = nums[fast] ;
+    }
+
+    return slow ;
+}
+
+
+// Runtime 128 ms Beats 25.64%
+// Memory 61.1 MB Beats 99.35%
 int findDuplicate(vector<int>& nums) 
 {
     int dup = -1 ;
