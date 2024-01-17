4e13a4930f9576aa4e80770f1befc0b2668de8db
Aman Kumar <amanalphakumar@gmail.com>
2024-01-18 00:16:53 +0530

    Update Unique_Number_of_Occurences.cpp

--- U/Unique_Number_of_Occurences.cpp
+++ U/Unique_Number_of_Occurences.cpp
@@ -2,23 +2,26 @@
 // https://leetcode.com/problems/unique-number-of-occurrences/
 // Aman Kumar
 
+// Runtime 0ms Beats100.00%of users with C++
+// Memory 9.18MB Beats9.43%of users with C++
 bool uniqueOccurrences(vector<int>& arr) 
 {
-    vector<int> freq( 2001, 0 ) ;
-
+    vector<int> freq(2001, 0) ;
+    
     for( int i = 0 ; i < arr.size() ; i++ )
+        freq[arr[i] +1000 ]++ ;
+    
+    vector<bool> hash(1001,0);
+    
+    for(int i = 0 ; i < 2001 ; i++ )
     {
-        freq[ arr[i] +1000 ]++ ;
+        if(freq[i] != 0)
+        {
+            if(hash[ freq[i] ] != 0)
+                return false;
+            hash[freq[i]] = 1;
+        }
     }
 
-    unordered_map<int,bool> hash ;
-
-    for( int i = 0 ; i < 2001 ; i++ )
-    {
-        if( freq[i] != 0 && hash[ freq[i] ] > 0 )
-            return false ;
-        else
-            hash[ freq[i] ] = 1 ;
-    }
     return true ;
 }
