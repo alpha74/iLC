dcc169286448d59b9de0b95a774c9ed982c6c830
Aman Kumar <amanalphakumar@gmail.com>
2024-04-17 23:32:32 +0530

    Update Smallest_String_Starting_From_Leaf.cpp

--- S/Smallest_String_Starting_From_Leaf.cpp
+++ S/Smallest_String_Starting_From_Leaf.cpp
@@ -2,8 +2,8 @@
 // https://leetcode.com/problems/smallest-string-starting-from-leaf/description/
 // Aman Kumar
 
-// Runtime 9 ms Beats 88.68%
-// Memory 20.3 MB Beats 14.83%
+// Runtime 8ms Beats65.38%of users with C++
+// Memory 20.87MB Beats29.76%of users with C++
 string getStringChar(int &n)
 {
     string ret = "" ;
@@ -14,19 +14,20 @@ string getStringChar(int &n)
 
 void checkSmallest( TreeNode *root, vector<string> &strList, string curr)
 {
-    if( root == NULL )
+    if(root == NULL)
         return ;
-
-    curr = getStringChar( root -> val ) + curr ;
-
-    if( root -> left == NULL && root -> right == NULL )
+    
+    curr = getStringChar(root -> val) + curr ;
+    
+    // Store string when leaf node
+    if(root -> left == NULL && root -> right == NULL)
     {
-        strList.push_back( curr ) ;
+        strList.push_back(curr) ;
         return ;
     }
 
-    checkSmallest( root -> left, strList, curr ) ;
-    checkSmallest( root -> right, strList, curr ) ;
+    checkSmallest(root -> left, strList, curr) ;
+    checkSmallest(root -> right, strList, curr) ;
 }    
 
 string smallestFromLeaf(TreeNode* root) 
@@ -34,23 +35,8 @@ string smallestFromLeaf(TreeNode* root)
     vector<string> strList ;
     string curr = "" ;
 
-    checkSmallest( root, strList, curr ) ;
-
-    sort( strList.begin(), strList.end() );
+    checkSmallest(root, strList, curr);
+    sort(strList.begin(), strList.end());
 
     return strList[0] ;
-
-/*
-[4,0,1,1]
-[0,1,3,4,3,4]
-[0]
-
-[0,1]
-[0,1,null,4]
-[4,0,0,1]
-
-[25,1,3,1,3,0,2,0,1,3,4,3,4]
-[2,2,1,null,1,0,null,0,0,1,3,4,3,4] 
-[25,1,null,0,0,1,null,null,null,0]
-*/
 }
