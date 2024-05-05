2301b0f02851f4dd68bf23a3a011dfd6119c4871
Aman Kumar <amanalphakumar@gmail.com>
2024-05-05 14:55:13 +0530

    Update and rename Delete_A_Node_In_Linked_List.cpp to Delete_Node_In_Linked_List.cpp

--- D/Delete_Node_In_Linked_List.cpp
+++ D/Delete_Node_In_Linked_List.cpp
@@ -2,14 +2,14 @@
 // https://leetcode.com/problems/delete-node-in-a-linked-list/submissions/
 // Aman Kumar
 
+// Runtime 3ms Beats92.47%of users with C++
+// Memory 11.26MB Beats84.10%of users with C++
 void deleteNode(ListNode* node) 
 {
-    if( node -> next == NULL )
-    {
-        delete node ;
-    }
-    node -> val = node -> next -> val ;
+    // Copy value and delete next node
     ListNode *del = node -> next ;
+    node -> val = del -> val ;
     node -> next = del -> next ;
+    
     delete del ;
 }
