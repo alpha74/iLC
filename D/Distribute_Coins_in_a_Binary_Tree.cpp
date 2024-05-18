61d2c65dd334988562f1db430f724a614a083f57
Aman Kumar <amanalphakumar@gmail.com>
2024-05-19 00:09:38 +0530

    Update Distribute_Coins_in_a_Binary_Tree.cpp

--- D/Distribute_Coins_in_a_Binary_Tree.cpp
+++ D/Distribute_Coins_in_a_Binary_Tree.cpp
@@ -6,24 +6,23 @@
 // Memory 13.9 MB Beats 51.77%
 int getExtraCoins( TreeNode *root, int &ret )
 {
-    if( root == NULL )
+    if(root == NULL)
         return 0 ;
-
-    int leftCoins = getExtraCoins( root -> left, ret ) ;
-    int rightCoins = getExtraCoins( root -> right, ret ) ;
-
-    // The number of movements will be abs sum of left and right extra coins
-    ret += abs( leftCoins ) + abs( rightCoins ) ;
-
-    // To understand, consider a simple tree with a left node and right node having 0 coins and root having 3 coins.
+    
+    int leftCoins = getExtraCoins(root -> left, ret) ;
+    int rightCoins = getExtraCoins(root -> right, ret) ;
+    
+    // The number of movements will be abs sum of left and right part
+    ret += abs(leftCoins) + abs(rightCoins) ;
+    
+    // To understand, consider a simple tree 
+    // with a left node and right node having 0 coins and root having 3 coins.
     return root -> val + leftCoins  + rightCoins - 1 ;
 }
 
 int distributeCoins(TreeNode* root) 
 {
     int ret = 0 ;
-
-    getExtraCoins( root, ret ) ;
-
+    getExtraCoins(root, ret) ;
     return ret ;
 }
