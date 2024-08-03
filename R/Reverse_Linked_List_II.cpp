c8492fabc910b24daf55058369681da57ae60d38
Aman Kumar <amanalphakumar@gmail.com>
2024-08-03 18:17:31 +0530

    Create Reverse_Linked_List_II.cpp

---
+++ R/Reverse_Linked_List_II.cpp
@@ -0,0 +1,80 @@
+// Reverse linked list given start and end index
+// https://leetcode.com/problems/reverse-linked-list-ii/description/
+// Aman Kumar
+
+// Runtime 0ms Beats 100.00% 
+// Memory 10.49MB Beats 28.90%
+ListNode* reverseList(ListNode *head)
+{
+    cout << "\n head: " << head -> val;
+    if(head == NULL || head -> next == NULL) return head;
+
+    ListNode *first = NULL;
+    ListNode *second = head;
+    ListNode *third = head -> next;
+
+    while(second != NULL)
+    {
+        second -> next = first;
+        first = second;
+        second = third;
+
+        if(third != NULL) third = third -> next;
+    }
+
+    return first;
+}
+
+ListNode* reverseBetween(ListNode* head, int left, int right) 
+{
+    ListNode *pStart = NULL;
+    ListNode *pEnd = NULL;
+    
+    ListNode *pStartPrev = NULL;
+    ListNode *pEndNext = head;
+
+    // New head will be returned
+    if(left == 1)
+    {
+        pStart = head;
+
+        // Find pEnd
+        pEnd = head;
+
+        right--;
+        while(right--)
+            pEnd = pEnd -> next;
+
+        pEndNext = pEnd -> next;
+        pEnd -> next = NULL;
+        
+        reverseList(pStart);
+
+        pStart -> next = pEndNext;
+        return pEnd;
+    }
+
+    left -=2 ;
+    pStartPrev = head;
+
+    while(left--)
+        pStartPrev = pStartPrev -> next;
+
+    pStart = pStartPrev -> next;
+
+    right--;
+    pEnd = head;
+    
+    while(right--)
+        pEnd = pEnd -> next;
+
+    pEndNext = pEnd -> next;
+    pEnd -> next = NULL;
+
+    reverseList(pStart);
+
+    pStart -> next = pEndNext;
+    pStartPrev -> next = pEnd;
+    
+    return head;
+}
