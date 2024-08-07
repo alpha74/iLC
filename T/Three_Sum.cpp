c6476c268349b30ba3c8871f99d3c8e05a13dda1
Aman Kumar <amanalphakumar@gmail.com>
2024-08-07 20:29:04 +0530

    Update Three_Sum.cpp

--- T/Three_Sum.cpp
+++ T/Three_Sum.cpp
@@ -2,53 +2,51 @@
 // https://leetcode.com/problems/3sum/
 // Aman Kumar
 
+// Runtime 56ms Beats 84.23%
+// Memory 27.42MB Beats 66.02%
 vector<vector<int>> threeSum(vector<int>& nums) 
 {
     int size = nums.size();
-
     sort(nums.begin(),nums.end());
-
+    
     vector<vector<int>> ret;
-
-    for(int i = 0 ; i < size ; i++ )
+    
+    // Three pointers approach
+    for(int i = 0 ; i < size ; i++)
     {
         int low = i+1, high = size-1 ;
-
-        while( low < high )
+        
+        while(low < high)
         {
             int sum = nums[i] + nums[low] + nums[high] ;
-
-            if(sum == 0 )
+            
+            if(sum == 0)
             {
                 ret.push_back({ nums[i], nums[low], nums[high] });
                 low++;
                 high--;
-
-                // Skip the duplicates from left to right
-                while( low < high && nums[low] == nums[low-1] )
-                {
+                
+                // Skip duplicates
+                while(low < high && nums[low] == nums[low-1])
                     low++;
-                }
-                // skip duplicates from right to left
+                
+                // Skip duplicates
                 while(high>low && nums[high] == nums[high+1] )
-                {
                     high--;
-                }
+                
             }
             else if( sum < 0 )
                 low++;
-
             else
                 high--;
-
         }
-
-        // Skip duplicates from left to right
-        while(i+1 < size && nums[i+1] == nums[i] )
+        
+        // Skip duplicates
+        while(i+1 < size && nums[i+1] == nums[i])
         {
             i++;
         }
     }    
-
+    
     return ret;
 }
