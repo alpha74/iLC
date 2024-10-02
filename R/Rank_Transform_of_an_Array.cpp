ffde1a0c1a5682ea882332dec422d6faedaa2771
Aman Kumar <amanalphakumar@gmail.com>
2024-10-02 22:38:13 +0530

    Update Rank_Transform_of_an_Array.cpp

--- R/Rank_Transform_of_an_Array.cpp
+++ R/Rank_Transform_of_an_Array.cpp
@@ -2,36 +2,37 @@
 // https://leetcode.com/problems/rank-transform-of-an-array/
 // Aman Kumar
 
+// Runtime 62ms Beats 78.39%
+// Memory 42.36MB Beats 77.16%
 vector<int> arrayRankTransform(vector<int>& arr) 
-{
-    vector<int> nums = arr ;
-
-    if( arr.size() == 0 )
+{   
+    if(arr.size() == 0)
     {
         vector<int> temp ;
         return temp ;
     }
 
-    sort( nums.begin(), nums.end() ) ;
-
+    vector<int> nums = arr ;
+    sort(nums.begin(), nums.end()) ;
+    
     unordered_map<int,int> hash ;
     int rank = 1 ;
-
+    
     hash[ nums[0] ] = rank ;
-
-    for( int i = 1 ; i < nums.size() ; i++ )
+    
+    for(int i = 1 ; i < nums.size() ; i++)
     {
-        if( nums[i] != nums[i-1] )
+        if(nums[i] != nums[i-1])
             rank++ ;
-
+        
         hash[ nums[i] ] = rank ;
     }
     nums.clear() ;
-
-    for( int i = 0 ; i < arr.size() ; i++ )
+    
+    for(int i = 0 ; i < arr.size() ; i++)
     {
-        nums.push_back( hash[ arr[i] ] ) ;
+        nums.push_back(hash[ arr[i] ]) ;
     }
-
+    
     return nums ;
 }
